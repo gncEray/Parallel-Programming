fb51e02fa8ca592282e0020acb1db3ffb83f7295
Eray <furkaneraygenc@gmail.com>
2023-03-08 15:38:45 +0300

    homework1

---
+++ hw1/19050111005.c
@@ -0,0 +1,8 @@
+#include <hellomake.h>
+
+int main() {
+
+	myFunc();
+
+	return 0;
+}
---
+++ hw1/hellofunc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <hellomake.h>
+
+void myFunc(void) {
+
+	printf("19050111005\n");
+
+	return;
+
+}
+
