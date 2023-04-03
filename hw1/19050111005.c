1eef48bf6357a294b902e5cfa725a7d4851d0319
Eray <furkaneraygenc@gmail.com>
2023-04-04 00:02:38 +0300

    hw3

--- hw1/19050111005.c
+++ hw1/19050111005.c
@@ -1,98 +1,98 @@
+// Eray Genç 19050111005
+
 #include "hellomake.h"
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
+
+
 
 int main(int argc, char *argv[]) {
 
- 	//myFunc();
+	// mpirun -n 3 ./your_id 1003 1003 output.txt
+    // 1,2-> 1003, 3-> output.txt
 
-	if (argc != 4) {
-		printf("You typed wrongly. Please give four arguments. Example form:\n");
-         printf("%s row_number(int) col_number(int) output_filename(string)\n", argv[0]);
-         return 1;
-     }
+    int rank, size;
+    //size = atoi(argv[2]);
 
-	//printf("String: %s\n", argv[0]);
-	//printf("String: %s\n", argv[1]);
-	//printf("String: %s\n", argv[2]);
-	//printf("String: %s\n", argv[3]);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-	// taking given inputs and initializing them to variables
-	int num1 = atoi(argv[1]); // I used atoi to convert string to integer
-	int num2 = atoi(argv[2]);
-	char *filename = argv[3];
-
-	long int seed = 19050111005; // my number as seed value
-	int i,j; // for iterations
-	double **matrix;
+    int MATRIX_SIZE = atoi(argv[2]);
+    long int seed = 19050111005;  // seed value as my number
+    double **matrix;
 	double *vector;
+    char *filename = argv[3];
+    FILE *output_file;
+    int i, j;  // for iterations
+    double start_time, end_time;
 
 
-	// creating and allocating memory for matrix	
-	matrix = (double **)malloc(num1 * sizeof(double *));
-
-	for(i = 0; i < num1; i++) {
-		matrix[i] = (double *)malloc(num2 * sizeof(double));
-	}
-
-	// seed value to my number
+    // seed value to my number
 	srand(seed);
 
+    // creating and allocating memory for matrix	
+	matrix = (double **)malloc(MATRIX_SIZE * sizeof(double *));
+	for(i = 0; i < MATRIX_SIZE; i++) {
+		matrix[i] = (double *)malloc(MATRIX_SIZE * sizeof(double));
+	}
+
 	// filling matrix with random double values between [1.0 - 100.0]
-//	printf("\n*****Matrix*****\n");
-	for(i = 0; i < num1; i++) {
-		for(j = 0; j < num2; j++) {
+	for(i = 0; i < MATRIX_SIZE; i++) {
+		for(j = 0; j < MATRIX_SIZE; j++) {
 			matrix[i][j] = (rand()) % 100 + 1.0; // taking random values between 1.0 and 100.0
-//			printf("%.1f ", matrix[i][j]); 
 		}
-//		printf("\n");
 	}
 
-	printf("%dx%d matrix created and filled with random double numbers.\n", num1, num2);
-
-
-	// creating a vector for multiplication
-//	printf("\n*****Vector*****\n");
-	vector = (double *)malloc(num2 * sizeof(double *));
-	for (i = 0; i < num2; i++) {
+    // creating a vector for multiplication
+	vector = (double *)malloc(MATRIX_SIZE * sizeof(double *));
+	for (i = 0; i < MATRIX_SIZE; i++) {
 		vector[i] = (rand()) % 100 + 1.0;
-//		printf("%.1f ", vector[i]);
 	}
+    
 
-	printf("Random vector is created for matrix X vector multiplication.\n");
+    // calculating number of rows each process will take
+    int chunk_size = MATRIX_SIZE / size;
+    int start_row = rank * chunk_size;
+    int end_row = (rank + 1) * chunk_size;
 
+    // allocating memory for result vector
+    double local_result[MATRIX_SIZE];
 
-	// creating an output file to write result
-	FILE *out_file = fopen(filename, "w");
-
-	if(out_file == NULL) {
-		printf("%s cannot open!", filename);
-	}
-
-	// multiplication of matrix and vector and writing to output file
-	double mult = 0;
-	for(i = 0; i < num1; i++) {
-                for(j = 0; j < num2; j++) {
-                       mult += matrix[i][j] * vector[j];
-                }
-		fprintf(out_file, "%.1f\n", mult);
-		mult = 0;
+    // performing matrix-vector multiplication
+    for (int i = start_row; i < end_row; i++) {
+        local_result[i] = 0;
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            local_result[i] += matrix[i][j] * vector[j];
+        }
     }
 
-	printf("\nSuccessfully multiplied and written to %s file.\n\n", filename);
+    double result[MATRIX_SIZE];
 
-	fclose(out_file);
+    start_time = MPI_Wtime();
 
-	// freeing memory
-	for (i = 0; i < num1; i++) {
-         free(matrix[i]);
-     }
-	free(matrix);
-	free(vector);
+    MPI_Reduce(local_result, result, MATRIX_SIZE, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	
 
-	return 0;
+    output_file = fopen(filename, "w"); 
+    
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        fprintf(output_file, "%.2f\n", result[i]);
+    }
+
+    end_time = MPI_Wtime();
+	double elapsed_time = end_time - start_time;
+
+    printf("\nElapsed time is %lf seconds for parallel mxv with %d processes\n", elapsed_time, size);
+
+    
+    MPI_Finalize();
+    free(vector);
+    free(matrix);
+	fclose(output_file);
+
+    return 0;
 }
