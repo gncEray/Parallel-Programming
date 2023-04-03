aec9d7a126e657918062331b9f64be9cf63a19a3
gncEray <62249421+gncEray@users.noreply.github.com>
2023-04-04 00:26:05 +0300

    hw3

--- hw1/19050111005.c
+++ hw1/19050111005.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[]) {
 
-	// mpirun -n 3 ./your_id 1003 1003 output.txt
+    // mpirun -n 3 ./your_id 1003 1003 output.txt
     // 1,2-> 1003, 3-> output.txt
 
     int rank, size;
@@ -24,34 +24,35 @@ int main(int argc, char *argv[]) {
     int MATRIX_SIZE = atoi(argv[2]);
     long int seed = 19050111005;  // seed value as my number
     double **matrix;
-	double *vector;
+    double *vector;
     char *filename = argv[3];
     FILE *output_file;
     int i, j;  // for iterations
     double start_time, end_time;
+    double result[MATRIX_SIZE];
 
 
     // seed value to my number
-	srand(seed);
+    srand(seed);
 
     // creating and allocating memory for matrix	
-	matrix = (double **)malloc(MATRIX_SIZE * sizeof(double *));
-	for(i = 0; i < MATRIX_SIZE; i++) {
-		matrix[i] = (double *)malloc(MATRIX_SIZE * sizeof(double));
-	}
+    matrix = (double **)malloc(MATRIX_SIZE * sizeof(double *));
+    for(i = 0; i < MATRIX_SIZE; i++) {
+	matrix[i] = (double *)malloc(MATRIX_SIZE * sizeof(double));
+    }
 
 	// filling matrix with random double values between [1.0 - 100.0]
-	for(i = 0; i < MATRIX_SIZE; i++) {
-		for(j = 0; j < MATRIX_SIZE; j++) {
-			matrix[i][j] = (rand()) % 100 + 1.0; // taking random values between 1.0 and 100.0
-		}
+    for(i = 0; i < MATRIX_SIZE; i++) {
+	for(j = 0; j < MATRIX_SIZE; j++) {
+		matrix[i][j] = (rand()) % 100 + 1.0; // taking random values between 1.0 and 100.0
 	}
+    }
 
     // creating a vector for multiplication
-	vector = (double *)malloc(MATRIX_SIZE * sizeof(double *));
-	for (i = 0; i < MATRIX_SIZE; i++) {
-		vector[i] = (rand()) % 100 + 1.0;
-	}
+    vector = (double *)malloc(MATRIX_SIZE * sizeof(double *));
+    for (i = 0; i < MATRIX_SIZE; i++) {
+	vector[i] = (rand()) % 100 + 1.0;
+    }
     
 
     // calculating number of rows each process will take
@@ -70,8 +71,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    double result[MATRIX_SIZE];
-
     start_time = MPI_Wtime();
 
     MPI_Reduce(local_result, result, MATRIX_SIZE, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -84,7 +83,7 @@ int main(int argc, char *argv[]) {
     }
 
     end_time = MPI_Wtime();
-	double elapsed_time = end_time - start_time;
+    double elapsed_time = end_time - start_time;
 
     printf("\nElapsed time is %lf seconds for parallel mxv with %d processes\n", elapsed_time, size);
 
@@ -92,7 +91,7 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     free(vector);
     free(matrix);
-	fclose(output_file);
+    fclose(output_file);
 
     return 0;
 }
